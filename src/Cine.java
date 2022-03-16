490a302434562231a6dfea9ddf3d9d9fe563eecc
LeoRosher <101595799+LeoRosher@users.noreply.github.com>
2022-03-15 21:31:38 -0400

    Update Cine.java
    Se agrega la clase

--- src/Cine.java
+++ src/Cine.java
@@ -1,2 +1,41 @@
 public class Cine {
+  String nombre;
+    int nit;
+    String direccion;
+    ArrayList<Sala> salas;
+    ArrayList<Pelicula> peliculas;
+    ArrayList<Empleado> empleados;
+
+    public Cine (String nombre, int nit, String direccion){
+        this.nombre = nombre;
+        this.nit = nit;
+        this.direccion = direccion;
+        this.peliculas = new ArrayList<Pelicula>();
+        this.salas = new ArrayList<Sala>();
+        this.empleados = new ArrayList<Empleado>();
+    }
+  
+  public void getAddSala(){
+        salas.add(sala);
+    }
+
+    public void getAddPelicula(){
+        peliculas.add(pelicula);
+    }
+
+    public void getAddEmpleado(){
+        empleados.add(empleado);
+    }
+
+    public int getSalaSize(){
+        return salas.size();
+    }
+
+    public int getPeliculasSize(){
+        return peliculas.size();
+    }
+
+    public int getEmpleadosSize(){
+        return empleados.size();
+    }
 }
