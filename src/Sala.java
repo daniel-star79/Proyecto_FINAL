47c846ac2992ac01e9b7f5986bbf998c7135cf23
Daniel Espinoza <danii003559@gmail.com>
2022-03-17 10:01:20 -0400

    Se modifico Sala y se Añadio un Test

--- src/Sala.java
+++ src/Sala.java
@@ -1,33 +1,54 @@
+import java.util.ArrayList;
+
 public class Sala {
 
         // write your code here
 
-        String IDSala = null;
-        Asientos asientos;
+        String IDSala;
+        ArrayList<Asientos> listaAsientos;
+        int capacidad = 100;
 
-// matrises
 
-    public Sala(String IDSala, Asientos asientos){
+    public Sala(String IDSala, int capacidad){
             this.IDSala = IDSala;
-            this.asientos = asientos;
+            this.capacidad = capacidad;
+            listaAsientos = new ArrayList<>();
         }
 
         public String getIDSala () {
             return IDSala;
         }
 
-
-        public Asientos getAsientos () {
-            return asientos;
-        }
-
         public void setIDSala (String IDSala){
             this.IDSala = IDSala;
         }
-        
 
-        public void setAsientos (Asientos asientos){
-            this.asientos = asientos;
+        public String CrearAsientos(){
+                int filas  = capacidad / 10 , count = 0 ;
+                String letraFila  = "A", asientos= "";
+                for(int i = 1 ;i<11;i++){
+                    listaAsientos.add(new Asientos(letraFila,i));
+                    asientos+= "["+listaAsientos.get(count).numeroAciento+" "+listaAsientos.get(count).letraFila+
+                            " "+listaAsientos.get(count).estado+"]";
+                    count ++;
+                    if(i==10) {
+                        asientos+="\n";
+                        i = 0;
+                        char codigo = letraFila.charAt(0);
+                        codigo++;
+                        letraFila = String.valueOf(codigo);
+                        filas--;
+                    }
+                    if (filas ==0){
+                        break;
+                    }
+                }
+            return asientos;
         }
+
+
+
+
+
     }
 
